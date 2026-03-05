8d7a244bdb390469870ca720de15a51702bc9839
Tapiwanashe Shoshore <tapsshore@gmail.com>
2026-03-05 09:13:47 +0200

    Initial project setup with Spring Boot 3.2
      - Configure Maven with Spring Boot 3.2.3 parent
      - Add dependencies: Web, JPA, PostgreSQL, Validation, Lombok, Actuator
      - Create main application class
      - Add basic application properties
      - Include standard .gitignore for Java/Maven projects

---
+++ src/main/java/za/co/capitecbank/fraud_rule_engine/FraudRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package za.co.capitecbank.fraud_rule_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudRuleEngineApplication.class, args);
+	}
+
+}
